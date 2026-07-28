0acab9565387341afb15ac5da4e854c0aec9a22d
Pham Le Triet Giang <pltgiang.degoholding@gmail.com>
2026-07-28 08:24:15 +0700

    Fix CORS and ngrok browser warning

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -11,6 +11,7 @@ export const api = axios.create({ baseURL })
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
   if (token) config.headers.Authorization = `Bearer ${token}`
+  config.headers['ngrok-skip-browser-warning'] = '69420'
   return config
 })
 
